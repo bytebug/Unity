25cb6849fc0437a43dfa0e0331d2a5b89591139c
bytebug <afterbnr99@msn.com>
2014-12-11 01:30:57 +0900

    Changed resolution to 600x900 Added some UI elements
    TO-DO:
    1. Fix UI bug
    2. Make "start" menu to work
    3. Add no-of-lives-left management
    4. Add enemy waves
    5. Change main assets (ships & enemies)
    6. Add level management system

---
+++ SpaceShooter/Assets/UIController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+	public Text startText;
+	public float speed;
+	Color flashingColor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		flashingColor.a = Mathf.PingPong (Time.time * speed, 1.0f);
+
+		startText.material.color = flashingColor;
+			//.SetColor("Text_Start", flashingColor);
+	
+	}
+}
